d50e14c4f39438a30eb08b6cd16bccd5482d0bb4
Samuel Cuerrier <6297553@cstjean.qc.ca>
2025-08-26 13:32:22 -0400

    changement de noms de classes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let reponseDate = document.getElementById("reponse-date");
 let reponseWhile = document.getElementById("reponse-while");
+let reponseDoWhile = document.getElementById("reponse-do-while");
 let WhileInput = document.getElementById("nb-while");
+let DoWhileInput = document.getElementById("nb-do-while");
 let dateInput = document.getElementById("date");
 const Mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
 
@@ -9,7 +11,6 @@ document.getElementById("form-date").onsubmit = function(event) {
     event.preventDefault();
     reponseDate.innerText = "Le nom du mois est : ";
     let d = (new Date(dateInput.value));
-    console.log(d.getMonth());
     let bonMois = Mois[d.getMonth()];
     reponseDate.innerHTML += bonMois;
 }
@@ -20,17 +21,24 @@ let formWhile = document.getElementById("form-while").
     reponseWhile.innerText = "Produit : ";
     let produit = 1;
     let compteur = 1;
-    console.log(WhileInput.value);
     while(compteur <= WhileInput.value)
     {
         produit = produit * compteur;
         compteur++;
-        console.log(produit);
     }
     reponseWhile.innerHTML += produit;
 }
 
 let formDoWhile = document.getElementById("form-do-while").
-    onsubmit = function() {
-    console.log("Do While");
+    onsubmit = function(event) {
+        event.preventDefault();
+        reponseDoWhile.innerText = "Produit : ";
+        let produit = 1;
+        let compteur = 1;
+        do
+        {
+            produit = produit * compteur;
+            compteur++;
+        }while(compteur <= DoWhileInput.value)
+        reponseDoWhile.innerHTML += produit;
 }
