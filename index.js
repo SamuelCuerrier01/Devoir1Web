6d690a3ee368dc452623cec50b099657cdb4afaf
Samuel Cuerrier <6297553@cstjean.qc.ca>
2025-08-25 21:57:15 -0400

    commit 1 manque les fonctions

---
+++ index.js
@@ -0,0 +1,17 @@
+let reponseDate = document.getElementById("reponseDate");
+let date = document.getElementById("");
+let formDate = document.getElementById("form-date").
+onsubmit = function() {
+    console.log("Date");
+}
+
+
+let formWhile = document.getElementById("form-while").
+    onsubmit = function() {
+    console.log("While");
+}
+
+let formDoWhile = document.getElementById("form-do-while").
+    onsubmit = function() {
+    console.log("Do While");
+}
